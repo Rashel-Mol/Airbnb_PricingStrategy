5e1f7a77c26c4c6fb527d22fbb24bd36b7df56c3
AmberVermeer <a.c.m.vermeer@tilburguniversity.edu>
2021-09-20 11:11:31 +0200

    code to downlaod the data

--- code/data_cleaning.R
+++ code/data_cleaning.R
@@ -1,1 +1,11 @@
 # Data Cleaning Codes 
+
+# Libraries
+library(ff)
+
+read.csv2("data/listings.csv")
+
+read.csv("data/reviews.csv")
+
+summary(listings)
+summary(reviews)
--- data/download_data.R
+++ data/download_data.R
@@ -8,18 +8,17 @@ library(dplyr)
 library(tidyr)
 library(googledrive)
 
-
 # --- Download Data --- #
 
-data_id <- "URL"
-originaldata_nl <- "data/listings.csv"
+data_id <- "https://drive.google.com/file/d/1t6M7rsIsB8G7_DsM47HY-EMMyAo2ep-f/view?usp=sharing"
+listings <- "data/listings.csv"
 drive_download(
   as_id(data_id),
   path = listings,
   overwrite = TRUE)
 
-data_id <- "URL"
-originaldata_nl <- "data/reviews.csv"
+data_id <- "https://drive.google.com/file/d/12-HJRBW1INHGQiOxaMLH64Y0woTC70Bn/view?usp=sharing"
+reviews <- "data/reviews.csv"
 drive_download(
   as_id(data_id),
   path = reviews,
