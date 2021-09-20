c576d50fc8ff2fc85b4a625a43964e56cead96ad
AstridR97 <78751109+AstridR97@users.noreply.github.com>
2021-09-20 15:00:37 +0200

    Update download_data.R
    Fixed problem that would not load in the files/

--- data/download_data.R
+++ data/download_data.R
@@ -3,6 +3,7 @@
 # --- Libraries --- #
 
 library(googledrive)
+library(readr)
 
 # --- Download Data --- #
 
@@ -10,12 +11,14 @@ data_id <- "https://drive.google.com/file/d/1t6M7rsIsB8G7_DsM47HY-EMMyAo2ep-f/vi
 listings <- "data/listings.csv"
 drive_download(
   as_id(data_id),
-  path = listings,
   overwrite = TRUE)
+
+listings <- read_csv("listings.csv")
 
 data_id <- "https://drive.google.com/file/d/12-HJRBW1INHGQiOxaMLH64Y0woTC70Bn/view?usp=sharing"
 reviews <- "data/reviews.csv"
 drive_download(
   as_id(data_id),
-  path = reviews,
   overwrite = TRUE)
+
+reviews <- read_csv("reviews.csv")
