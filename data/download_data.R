cbd64fe5fd869315e02facfd530b0afca4fe2cd2
AmberVermeer <a.c.m.vermeer@tilburguniversity.edu>
2021-09-20 10:22:53 +0200

    code to download data from google drive

--- data/download_data.R
+++ data/download_data.R
@@ -1,1 +1,26 @@
 # Download the data
+
+# --- Libraries --- #
+
+library(readr)
+library(tidygraph)
+library(dplyr)
+library(tidyr)
+library(googledrive)
+
+
+# --- Download Data --- #
+
+data_id <- "URL"
+originaldata_nl <- "data/listings.csv"
+drive_download(
+  as_id(data_id),
+  path = listings,
+  overwrite = TRUE)
+
+data_id <- "URL"
+originaldata_nl <- "data/reviews.csv"
+drive_download(
+  as_id(data_id),
+  path = reviews,
+  overwrite = TRUE)
