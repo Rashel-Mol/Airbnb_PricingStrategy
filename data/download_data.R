116baece3c85376adf7df865ad904bce4d7e1f98
AstridR97 <78751109+AstridR97@users.noreply.github.com>
2021-09-21 12:56:47 +0200

    improve downloading code: it is a loop now

--- data/download_data.R
+++ data/download_data.R
@@ -7,18 +7,15 @@ library(readr)
 
 # --- Download Data --- #
 
-data_id <- "https://drive.google.com/file/d/1t6M7rsIsB8G7_DsM47HY-EMMyAo2ep-f/view?usp=sharing"
-listings <- "data/listings.csv"
-drive_download(
-  as_id(data_id),
-  overwrite = TRUE)
+files = list("https://drive.google.com/file/d/1t6M7rsIsB8G7_DsM47HY-EMMyAo2ep-f/view?usp=sharing",
+             "https://drive.google.com/file/d/12-HJRBW1INHGQiOxaMLH64Y0woTC70Bn/view?usp=sharing")
 
-listings <- read_csv("listings.csv")
+for (f in files) {
+  cat(paste0('Downloading file: ', f, '...\n'))
+  
+  drive_download(
+    file = f,
+    overwrite = TRUE)
 
-data_id <- "https://drive.google.com/file/d/12-HJRBW1INHGQiOxaMLH64Y0woTC70Bn/view?usp=sharing"
-reviews <- "data/reviews.csv"
-drive_download(
-  as_id(data_id),
-  overwrite = TRUE)
+}
 
-reviews <- read_csv("reviews.csv")
